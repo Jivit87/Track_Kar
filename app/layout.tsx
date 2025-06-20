531da22259ad9fbbdbabafc5dbc1468ca1d1ae5b
Jivit87 <jivitrana87@gmail.com>
2025-06-20 18:46:23 +0530

    Added env

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script"; // ← add this
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 const spaceGrotesk = Space_Grotesk({
@@ -17,6 +17,8 @@ export const metadata: Metadata = {
     "Track product prices effortlessly and save money on your online shoppping.",
 };
 
+const GA_ID = process.env.NEXT_PUBLIC_GA_ID;
+
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -28,18 +30,15 @@ export default function RootLayout({
         {/* Google Analytics scripts */}
         <Script
           strategy="afterInteractive"
-          src="https://www.googletagmanager.com/gtag/js?id=G-V09RW9SZP0"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
         />
-        <Script
-          id="google-analytics"
-          strategy="afterInteractive"
-        >
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-V09RW9SZP0');
-          `}
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+  `}
         </Script>
       </head>
       <body className={inter.className}>
